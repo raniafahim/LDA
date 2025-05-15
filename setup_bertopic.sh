adca1a0937b96d0ec2e224347504c3c9a19eab78
raniafahim <fahim.rania24@gmail.com>
2025-05-15 13:43:02 +0000

    env python 3.11

--- setup_bertopic.sh
+++ setup_bertopic.sh
@@ -1,5 +1,6 @@
 cd ~/work/TopicModeling/ 
-uv venv .venv
+rm -rf .venv 
+/usr/local/bin/python3.11 -m venv .venv
 source .venv/bin/activate
 uv pip install notebook ipykernel
 python -m ipykernel install --user --name=my-uv-env --display-name "Python (uv-bt)"
